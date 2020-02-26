0ed18e4159952fff638a230689174138e3efbd5a
SamiraBelkhiri <belkhirisamira@hotmail.com>
2020-02-26 11:42:26 +0100

    pushing data to index

--- Model/connection.php
+++ Model/connection.php
@@ -2,20 +2,21 @@
 declare(strict_types=1);
 
 
+    function openConnection()
+    {
+        // Try to figure out what these should be for you
+        $dbhost = "localhost";
+        $dbuser = "samira";
+        $dbpass = "password";
+        $db = "becode";
 
-        function openConnection()
-        {
-            // Try to figure out what these should be for you
-            $dbhost = "localhost";
-            $dbuser = "samira";
-            $dbpass = "password";
-            $db = "becode";
+        // Try to understand what happens here
+        $pdo = new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-            // Try to understand what happens here
-            $pdo = new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // Why we do this here
+        return $pdo;
+    }
 
-            // Why we do this here
-            return $pdo;
-        }
+
--- index.php
+++ index.php
@@ -12,6 +12,36 @@ require 'Model/Post.php';
 
 $controller = new HomePageController();
 $controller->render($_POST);
+$pdo = openConnection();
 
+$statement = $pdo->query("SELECT first_name, last_name, email, preferred_language FROM student");
+foreach ($statement as $storedInformation):
 
+    ?>
 
+    <tr>
+        <td><h3>Information</h3></td>
+    </tr>
+    <tr>
+        <td>First name: </td>
+        <td><?php echo $storedInformation['first_name'] ?></td>
+    </tr>
+    <tr>
+        <td>Last name: </td>
+        <td><?php echo $storedInformation['last_name'] ?></td>
+    </tr>
+    <tr>
+        <td>Email: </td>
+        <td><?php echo $storedInformation['email'] ?></td>
+    </tr>
+    <tr>
+        <td>
+            <img src="<?php echo 'Images/' . $storedInformation['preferred_language'] . '.png' ?>" alt="flag">
+        </td>
+    </tr>
+    <tr>
+        <td><a href=""></a>Profile</td>
+    </tr>
+
+<?php endforeach; ?>
+</table>
